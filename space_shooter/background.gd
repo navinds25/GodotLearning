c869e7f53e8fd1ce04f453170f9da4248b789812
Navin Sadanandan <navinds25@gmail.com>
2024-10-13 17:32:28 +0530

    fixed instance error in space_shooter

--- space_shooter/background.gd
+++ space_shooter/background.gd
@@ -1,4 +1,4 @@
-extends Sprite
+extends Sprite2D
 
 const SCREEN_WIDTH = 320
 
--- space_shooter/stage.gd
+++ space_shooter/stage.gd
@@ -10,24 +10,24 @@ var score = 0
 
 func _ready():
 	# restart game
-	get_node("player").connect("destroyed", self, "_on_player_destroyed")
+	get_node("player").connect("destroyed", Callable(self, "_on_player_destroyed"))
 	# spawn asteroids
-	get_node("spawn_timer").connect("timeout", self, "on_spawn_timer_timeout")
+	get_node("spawn_timer").connect("timeout", Callable(self, "on_spawn_timer_timeout"))
 
 func _input(event):
 	if Input.is_key_pressed(KEY_ESCAPE):
 		get_tree().quit()
 	if is_game_over and Input.is_key_pressed(KEY_ENTER):
-		get_tree().change_scene("res://stage.tscn")
+		get_tree().change_scene_to_file("res://stage.tscn")
 
 func _on_player_destroyed():
 	get_node("ui/retry").show()
 	is_game_over = true
 
 func on_spawn_timer_timeout():
-	var asteroid_instance = asteroid.instance()
-	asteroid_instance.position = Vector2(SCREEN_WIDTH + 8, rand_range(0, SCREEN_HEIGHT))
-	asteroid_instance.connect("score", self, "_on_player_score")
+	var asteroid_instance = asteroid.instantiate()
+	asteroid_instance.position = Vector2(SCREEN_WIDTH + 8, randf_range(0, SCREEN_HEIGHT))
+	asteroid_instance.connect("score", Callable(self, "_on_player_score"))
 	add_child(asteroid_instance)
 
 func _on_player_score():
