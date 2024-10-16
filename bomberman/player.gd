7ecc9ed5b17b26374425410a291cca3e26270975
Navin <navinds25@gmail.com>
2024-10-16 23:48:51 +0530

    bomberman plyr init

---
+++ bomberman/player.gd
@@ -0,0 +1,26 @@
+extends CharacterBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if velocity.x != 0:
+		$AnimatedSprite2D.animation = "walk"
+		$AnimatedSprite2D.flip_v = false
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite2D.animation = "up"
+		$AnimatedSprite2D.flip_v = velocity.y > 0
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("space"):
+		print("action pressed")
+		
+	if event.is_action_released("space"):
+		print("action released")
+		
